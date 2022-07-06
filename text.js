9af966e041840966d864e247e4f359799e6fa058
minecraftpublisher <mforoud86@gmail.com>
2022-07-06 23:18:30 +0430

    Added a hashing method to messages. How vibrant!

--- text.js
+++ text.js
@@ -5,16 +5,42 @@ let sounds = ['/assets/sent.mp3', '/assets/received.mp3'];
 
 function iOS() {
     return [
-      'iPad Simulator',
-      'iPhone Simulator',
-      'iPod Simulator',
-      'iPad',
-      'iPhone',
-      'iPod'
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod'
     ].includes(navigator.platform)
-    // iPad on iOS 13 detection
-    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-  }
+        // iPad on iOS 13 detection
+        || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
+String.prototype.hashCode = function () {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+String.prototype.colorify = function () {
+    let hash = Math.abs(this.hashCode()).toString();
+    if (hash.length < 6) hash = (new Array(20)).join(hash).substring(0, 6);
+    return `#${hash.substring(0, 6)}`;
+};
+
+const reverse_hex = ((bgColor, lightColor = '#ffffff', darkColor = '#000000') => {
+    var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+    var r = parseInt(color.substring(0, 2), 16); // hexToR
+    var g = parseInt(color.substring(2, 4), 16); // hexToG
+    var b = parseInt(color.substring(4, 6), 16); // hexToB
+    return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186) ?
+        darkColor : lightColor;
+});
 
 /**
  * Writes a message to the screen and returns the dom element of that message.
@@ -23,7 +49,7 @@ function iOS() {
  * @param {string} author The author for received messages.
  * @returns {HTMLElement} The dom element of the message.
  */
-const message = globalThis.message = ((sent, input, author = ath, time = true) => {
+const message = globalThis.message = ((sent, input, author = ath, time = true, clickd = false) => {
     const scroll = (() => document.querySelector('main').scroll({ top: document.querySelector('main').scrollHeight, behavior: "smooth" }));
     /* yk */ input = input.toLowerCase();
 
@@ -39,6 +65,8 @@ const message = globalThis.message = ((sent, input, author = ath, time = true) =
     } else {
         const elm = document.createElement('wrapper');
         elm.classList.add(sent ? 'from' : 'to');
+        let background = author.toLowerCase().colorify();
+        let foreground = reverse_hex(background);
         const data = document.createElement('message');
         elm.appendChild(data);
         const text = document.createElement('text');
@@ -48,6 +76,48 @@ const message = globalThis.message = ((sent, input, author = ath, time = true) =
         // add the hour and the minute to the author. the minutes should be two digits.
         _author.innerHTML = (sent ? 'sent' : author) + (time ? ' ' + new Date().getHours() + ':' + (new Date().getMinutes() < 10 ? '0' : '') + new Date().getMinutes() : '');
         data.appendChild(_author);
+        if (!sent) {
+            background = author.colorify();
+            foreground = reverse_hex(background);
+            elm.style.background = background;
+            elm.style.color = foreground;
+            data.style.background = background;
+            data.style.color = foreground;
+            data.style.border = `1px solid ${foreground}`;
+            text.style.background = background;
+            text.style.color = foreground;
+            _author.style.background = background;
+            _author.style.color = foreground;
+
+            if (clickd) {
+                data.onmouseover = (() => {
+                    // lighten the background color
+                    elm.style.background = foreground;
+                    data.style.background = foreground;
+                    text.style.color = background;
+                    _author.style.color = background;
+                    text.style.background = foreground;
+                    _author.style.background = foreground;
+                    data.style.border = `1px solid ${background}`;
+                    data.style.transition = 'color 0.2s ease-in-out, background 0.2s ease-in-out';
+                    text.style.transition = 'color 0.2s ease-in-out, background 0.2s ease-in-out';
+                    _author.style.transition = 'color 0.2s ease-in-out, background 0.2s ease-in-out';
+                });
+                data.onmouseout = (() => {
+                    // darken the background color
+                    elm.style.background = background;
+                    data.style.background = background;
+                    text.style.color = foreground;
+                    _author.style.color = foreground;
+                    text.style.background = background;
+                    _author.style.background = background;
+                    data.style.border = `1px solid ${foreground}`;
+                    data.style.transition = 'color 0.2s ease-in-out, background 0.2s ease-in-out';
+                    text.style.transition = 'color 0.2s ease-in-out, background 0.2s ease-in-out';
+                    _author.style.transition = 'color 0.2s ease-in-out, background 0.2s ease-in-out';
+                });
+            }
+        }
 
         document.querySelector('main').appendChild(elm);
         if (playsound && !iOS()) {
