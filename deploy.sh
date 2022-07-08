57943d4815ad1f610136fe3803e745da0dfb9f62
minecraftpublisher <mforoud86@gmail.com>
2022-07-08 13:40:44 +0430

    Fixed last publish

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo Configuring times...
 # get current time
-time=$(($(date +%s%N)/1000000))
+time=$(date +%s%N)
 # write it to ./last-deploy as a timestamp with miliseconds
 rm -r last-deploy
 echo $time > last-deploy
